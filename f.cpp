04869b71c6c95da667ad12b082ac832fcd0049ac
Denis Barbier <barbier@imacs.polytechnique.fr>
2021-12-03 12:48:52 +0100

    Test with hypre

--- f.cpp
+++ f.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
+#include <mpi.h>
+#include <hypre/HYPRE_struct_ls.h>
+
 #include "f.hpp"
 
 void f()
 {
-  std::cout << "Hello world!\n";
+    std::cout << "Hello world!\n";
+    HYPRE_StructGrid grid;
+    int ndim        = 2;
+    int ilower[][2] = {{-3,1}, {0,1}};
+    int iupper[][2] = {{-1,2}, {2,4}};
+
+    /* Create the grid object */
+    HYPRE_StructGridCreate(MPI_COMM_WORLD, ndim, &grid);
+
+    /* Set grid extents for the first box */
+    HYPRE_StructGridSetExtents(grid, ilower[0], iupper[0]);
+
+    /* Set grid extents for the second box */
+    HYPRE_StructGridSetExtents(grid, ilower[1], iupper[1]);
+
+    /* Assemble the grid */
+    HYPRE_StructGridAssemble(grid);
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <mpi.h>
 
 #include "f.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
+  MPI_Init(&argc, &argv);
   f();
+  MPI_Finalize();
 }
